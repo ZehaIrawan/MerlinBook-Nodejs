08de3ac90731894feb8a306f9f9d9dcf817cfe17
Zeha Irawan <jangkaringbumi@gmail.com>
2019-06-29 18:11:52 +0700

    Add get & delete for post, like, & comment routes

--- models/Post.js
+++ models/Post.js
@@ -42,13 +42,13 @@ const PostSchema = new Schema({
       },
       date: {
         type: Date,
-        default: Date.now
+        defaul: Date.now
       }
     }
   ],
   date: {
     type: Date,
-    default: Date.now
+    defaul: Date.now
   }
 });
 
