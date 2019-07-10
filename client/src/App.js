55ffbfbfdfea2443ab849e45b3311fb442f2987d
Zeha Irawan <jangkaringbumi@gmail.com>
2019-07-10 21:11:48 +0700

    set up react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, {Fragment} from 'react';
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
 import './App.css';
 
 const App =() => {
   return (
     <Fragment>
-      <h1>My App</h1>
+     <Navbar/>
+     <Landing/>
     </Fragment>
   );
 }
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Connecting the dots</h1>
+          <p className='lead'>
+       Share your ideas to make a world a better place, get opinion from the experts, validate your ideas
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
